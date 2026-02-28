93af309ff3104116a17122aa8687985e085b029f
Kenneth Fossen <fossen.kenneth@gmail.com>
2026-02-28 12:53:58 +0100

    qmk lint

--- keyboards/handwired/bouvet/macropad/v2/config.h
+++ keyboards/handwired/bouvet/macropad/v2/config.h
@@ -1,5 +1,5 @@
-#define BOOTMAGIC_ROW 0
-#define BOOTMAGIC_COLUMN 0
+// Copyright 2023 Kenneth Fossen
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #define ENCODER_A_PINS { GP7, GP16 }
 #define ENCODER_B_PINS { GP8, GP17 }
