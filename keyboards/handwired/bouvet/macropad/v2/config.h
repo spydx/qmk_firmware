b4306cd9e6a8968adf119828aa63f9ffa0aae8c3
Kenneth Fossen <fossen.kenneth@gmail.com>
2026-03-07 19:18:00 +0100

    Update encoder config

--- keyboards/handwired/bouvet/macropad/v2/config.h
+++ keyboards/handwired/bouvet/macropad/v2/config.h
@@ -1,5 +1,5 @@
 // Copyright 2023 Kenneth Fossen
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#define ENCODER_A_PINS { GP6, GP7 }
-#define ENCODER_B_PINS { GP15, GP26 }
+#define ENCODER_A_PINS { GP6, GP15 }
+#define ENCODER_B_PINS { GP7, GP26 }
